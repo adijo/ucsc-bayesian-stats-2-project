1e89d9cd691ebc2546a220300aed31cce3bffd02
Aditya Joshi <1adityajoshi@gmail.com>
2020-05-06 12:35:13 -0400

    Change dataset and code

--- src/clean.R
+++ src/clean.R
@@ -1,61 +1,19 @@
-# Source of script: https://github.com/christophM/interpretable-ml-book/blob/master/R/get-cervical-cancer-dataset.R
-# Dataset source: http://archive.ics.uci.edu/ml/datasets/Cervical+cancer+%28Risk+Factors%29
-# Paper: http://www.inescporto.pt/~jsc/publications/conferences/2017KelwinIBPRIA.pdf
+library("titanic")
+library("tidyr")
 
-library("dplyr")
-
-get.cervical.data = function(data_dir) {
-  cervical = read.csv(
-    sprintf('%s/risk_factors_cervical_cancer.csv', data_dir), 
-    na.strings = c('?'), 
-    stringsAsFactors = FALSE)
+get.titanic.data = function() {
   
-  cervical = select(cervical, -Citology, -Schiller, -Hinselmann)
+  # Drop unimportant columns (for now)
+  df <- subset(titanic_train, select = -c(PassengerId, Name, Cabin, Fare, Ticket))
+ 
+   # Omit NA's
+  df = na.omit(df, target.colnames = c("Age", "Embarked"))
   
-  cervical$Biopsy = factor(cervical$Biopsy, levels = c(0, 1), labels=c('Healthy', 'Cancer'))
-  
-  ## subset variables to the ones that should be used in the book
-  cervical = dplyr::select(cervical, 
-                           Age, 
-                           Number.of.sexual.partners, 
-                           First.sexual.intercourse,
-                           Num.of.pregnancies, 
-                           Smokes, 
-                           Smokes..years., 
-                           Hormonal.Contraceptives, 
-                           Hormonal.Contraceptives..years.,
-                           IUD, 
-                           IUD..years., 
-                           STDs, 
-                           STDs..number., 
-                           STDs..Number.of.diagnosis, 
-                           STDs..Time.since.first.diagnosis,
-                           STDs..Time.since.last.diagnosis, 
-                           Biopsy)
-  
-  # NA imputation
-  imputer = mlr::imputeMode()
-  
-  
-  cervical_impute = mlr::impute(cervical, classes = list(numeric = imputer))
-  cervical = cervical_impute$data
-  
-  
-  # Factor variables: Smokes, IUD, STDs, Hormonal.Contraceptives
-  cervical$Smokes = factor(cervical$Smokes, levels = c(0, 1), labels=c("Smoker", "Non Smoker"))
-  cervical$STDs = factor(cervical$STDs, levels=c(0, 1), labels=c("Has STDs", "No STDs"))
-  cervical$IUD = factor(cervical$IUD, levels = c(0, 1), labels=c("Has IUD", "No IUD"))
-  cervical$Hormonal.Contraceptives = factor(cervical$Hormonal.Contraceptives, levels = c(0, 1), labels=c("Contraceptives", "No Contraceptives"))
-  cervical <- setNames(
-    cervical, 
-    c("age", "num_sexual_partners", "first_sexual_intercourse", "num_pregnancies",
-      "smokes", "smokes_yrs", "hormonal_contraceptives", "hormonal_contraceptives_yrs",
-      "iud", "iud_yrs", "stds", "num_stds", "num_stds_diag", "first_time_std_diag",
-      "last_time_std_diag", "biopsy"))
-  cervical
-}
-
-get.cervical.task = function(data_dir){
-  cervical = get.cervical.data(data_dir)
-  mlr::makeClassifTask(id='cervical', data = cervical, target = 'Biopsy')
+  # Convert to factors
+  df$Survived = factor(df$Survived, levels = c(0, 1), labels = c("Survived", "Dead"))
+  df$Sex = factor(df$Sex, levels = c("male", "female"), labels = c("Male", "Female"))
+  df$Embarked = factor(df$Embarked, levels = c("S", "C", "Q"), labels = c("Southampton", "Cherbourg", "Queenstown"))
+  df$Pclass = factor(df$Pclass, levels = c(1, 2, 3), labels = c("First", "Second", "Third"))
+ 
+  na.omit(df)
 }
--- src/exploration.R
+++ src/exploration.R
@@ -1,55 +1,25 @@
 library("ggplot2")
 library("ggthemr")
 ggthemr("fresh")
-
-
 source("src/clean.R")
 source("src/utils.R")
 
-df = get.cervical.data("data")
-str(df)
+df = get.titanic.data()
+
+# Appears that more females died.
+ggplot(df, aes(x=Survived, fill=Sex)) + geom_bar()
+
+# In first class, the ones that survived were overwhelmingly men
+ggplot(df, aes(x=Survived, fill=Sex)) + geom_bar() + facet_grid(.~Pclass)
+
+# For people who died, the distributions between male and female look similar
+# For people who survived, the men seem to be older than females
+ggplot(df, aes(x=Survived, y=Age, fill=Sex)) + geom_boxplot()
 
 
-### Distribution of target variable
-
-# Simple plot
-ggplot(df, aes(x=biopsy)) + 
-  geom_bar(aes(y = (..count..) / sum(..count..))) + 
-  labs(y = "Percentage (%)",
-       title="Distribution")
+# Higher values of Parch combined with low values of Sibsp seem to indicate better survival
+# Similarly, higher values of SipSp with lower values of parch survived more.
+ggplot(df, aes(x=Parch, y=SibSp, color=Survived))  + geom_point()
 
 
-ggplot(df, aes(x=smokes, y=age, fill=biopsy)) + geom_boxplot() 
-
-ggplot(df, aes(x=hormonal_contraceptives, y=age, fill=biopsy)) + geom_boxplot() 
-
-# Potentially useful
-ggplot(df, aes(x=hormonal_contraceptives, y=age, fill=biopsy)) + geom_boxplot()  + facet_grid(.~smokes)  
-
-ggplot(df, aes(x=num_stds_diag, y=age, fill=biopsy)) + geom_boxplot()  + facet_grid(.~smokes) + labs(x = "Number of Diagnosed STDs", y="Age")
-
-# Potentialy useful
-ggplot(df, aes(x=num_pregnancies,y=hormonal_contraceptives_yrs, color=biopsy)) + geom_point()  + facet_grid(iud~smokes)
-
-
-# Potentially useful
-ggplot(df, aes(x=age, y=hormonal_contraceptives_yrs, color=biopsy)) + geom_point()  + facet_grid(stds~smokes)
-
-# Potentially useful: Need to find what explains high values for Hormonal contraceptives for healthy patients.
-ggplot(df, aes(x=iud_yrs, y=hormonal_contraceptives_yrs, color=biopsy)) + geom_point() 
-
-
- 
-# The following plot does a decent job of separating stuff, but there are still patients with high number of IUD years that don't have
-# the cancer. What can explain a high number of IUD years?
-ggplot(df, aes(x=iud_yrs, y=hormonal_contraceptives_yrs, color=biopsy)) + geom_point() + facet_grid(smokes~.)
-
-#Potentially useful
-ggplot(df, aes(x=hormonal_contraceptives_yrs, y=num_pregnancies, color=biopsy)) + geom_point()  + facet_grid(stds~.)
-
-ggplot(df, aes(x=num_pregnancies, y=hormonal_contraceptives_yrs, color=biopsy)) + geom_point()  + facet_grid(iud~smokes)
-
-ggplot(df, aes(x=last_time_std_diag, y=age, fill=biopsy)) + geom_boxplot()  + facet_grid(stds~.)  # interesting
-
-
-
+ggplot(df, aes(x=SibSp, y=Parch, color=Survived)) + geom_point() + facet_grid(.~Embarked)
